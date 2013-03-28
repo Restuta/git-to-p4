3644f6535a8cb0e6e6589817d9dbf8b793b44ad1
Anton Vynogradenko <anton.vynogradenko@frontrange.com>
2013-03-28 16:54:41 -0700

    add commit parsing, prints p4 changeset id

--- p4-sync-to-latest-changeset-that-is-in-git.ps1
+++ p4-sync-to-latest-changeset-that-is-in-git.ps1
@@ -2,10 +2,18 @@ $Env:P4CHARSET = "utf8"
 $currentDir = Get-Location
 
 git ch p4
-$changelistTag = git tag --points-at HEAD
+
+$lastCommitMessage = (git log --pretty=format:'%B' HEAD^..HEAD)[0] #getting first line, since the result is array of lines
+
+if ($lastCommitMessage -Match "@(?<changesetId>\d+)") {
+    Write-Host  $matches["changesetId"] -ForegroundColor Red   
+}
+
+Write-Host $lastCommitMessage
+
 Write-Host "Latest changelist git was updated to is: " -NoNewLine
 Write-Host $changelistTag -ForegroundColor Yellow
 
 Write-Host "Running: " -NoNewLine -ForegroundColor DarkYellow
 Write-Host "p4 sync -q $(`"$currentDir...`" + $changelistTag)" -ForegroundColor Yellow
-p4 sync -q ("$currentDir..." + $changelistTag)
+#p4 sync -q ("$currentDir..." + $changelistTag)
