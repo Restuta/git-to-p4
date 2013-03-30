b9f8cd5ca970ee271cd374f122b75c8d182005af
Anton Vynogradenko <anton.vynogradenko@frontrange.com>
2013-03-29 19:32:19 -0700

    updated to reflect changes that p4 changeset id is now part of the commit message and not tag

--- p4-sync-to-latest-changeset-that-is-in-git.ps1
+++ p4-sync-to-latest-changeset-that-is-in-git.ps1
@@ -6,14 +6,12 @@ git ch p4
 $lastCommitMessage = (git log --pretty=format:'%B' HEAD^..HEAD)[0] #getting first line, since the result is array of lines
 
 if ($lastCommitMessage -Match "@(?<changesetId>\d+)") {
-    Write-Host  $matches["changesetId"] -ForegroundColor Red   
+    $changesetId = $matches["changesetId"]
 }
 
-Write-Host $lastCommitMessage
-
 Write-Host "Latest changelist git was updated to is: " -NoNewLine
-Write-Host $changelistTag -ForegroundColor Yellow
+Write-Host $changesetId -ForegroundColor Yellow
 
 Write-Host "Running: " -NoNewLine -ForegroundColor DarkYellow
-Write-Host "p4 sync -q $(`"$currentDir...`" + $changelistTag)" -ForegroundColor Yellow
-#p4 sync -q ("$currentDir..." + $changelistTag)
+Write-Host "p4 sync -q $(`"$currentDir...`" + "@" + $changesetId)" -ForegroundColor White
+p4 sync -q ("$currentDir..." + "@" + $changesetId)
